cebab04c71dd0e482a34f292a3a9f48532eca7dd
Shengbi <39143915+Countonme@users.noreply.github.com>
2026-06-05 05:20:46 +0800

    update layout
    Co-authored-by: Shengbi <39143915+Countonme@users.noreply.github.com>

--- HuyenRS232ToKeyboardTool/Form1.Designer.cs
+++ HuyenRS232ToKeyboardTool/Form1.Designer.cs
@@ -115,7 +115,7 @@
             // 
             this.tabPage1.Location = new System.Drawing.Point(201, 0);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(551, 376);
+            this.tabPage1.Size = new System.Drawing.Size(551, 459);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Main";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -570,7 +570,7 @@
             this.uiCheckBox17.Padding = new System.Windows.Forms.Padding(22, 0, 0, 0);
             this.uiCheckBox17.Size = new System.Drawing.Size(153, 35);
             this.uiCheckBox17.TabIndex = 53;
-            this.uiCheckBox17.Text = "2";
+            this.uiCheckBox17.Text = "WinPostMessage";
             // 
             // uiCheckBox18
             // 
