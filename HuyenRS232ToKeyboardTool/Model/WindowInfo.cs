c5dd590d84fe5d6d3ec2563e9c2e471a9206c2f2
Shengbi <39143915+Countonme@users.noreply.github.com>
2026-06-07 23:26:13 +0800

    #调整架构

---
+++ HuyenRS232ToKeyboardTool/Model/WindowInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class WindowInfo
+{
+    public IntPtr Hwnd { get; set; }
+
+    public string Title { get; set; }
+
+    public string ClassName { get; set; }
+
+    public string ProcessName { get; set; }
+
+    public uint PID { get; set; }
+}
--- HuyenRS232ToKeyboardTool/Services/WindowHelper.cs
+++ HuyenRS232ToKeyboardTool/Services/WindowHelper.cs
@@ -76,15 +76,3 @@ public static class WindowHelper
     }
 }
 
-public class WindowInfo
-{
-    public IntPtr Hwnd { get; set; }
-
-    public string Title { get; set; }
-
-    public string ClassName { get; set; }
-
-    public string ProcessName { get; set; }
-
-    public uint PID { get; set; }
-}
